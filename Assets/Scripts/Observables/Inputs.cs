84b538d0edc76b89d207068af8bc8853226edb97
Yunspace <yun@yunspace.com>
2017-01-15 14:03:22 +1100

    added runValue toggle

--- Assets/Scripts/Observables/Inputs.cs
+++ Assets/Scripts/Observables/Inputs.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 namespace Assets.Scripts.Observables
 {
-    public class Inputs : MonoBehaviour {
-
+    public class Inputs : MonoBehaviour
+    {
         public IObservable<Vector2> Movement { get; private set; }
         public IObservable<Vector2> Mouselook { get; private set; }
         public ReadOnlyReactiveProperty<bool> Run { get; private set; }
+
+        private bool _runValue;
 
         private void Awake()
         {
@@ -16,23 +18,39 @@ namespace Assets.Scripts.Observables
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
-            Movement = this.FixedUpdateAsObservable()
-                .Select(_ => {
+            Movement = MovementObservable();
+            Mouselook = MouseObservable();
+            Run = RunFlag();
+        }
+
+        private IObservable<Vector2> MovementObservable()
+        {
+            return this.FixedUpdateAsObservable()
+                .Select(_ =>
+                {
                     var x = Input.GetAxis("Horizontal");
                     var y = Input.GetAxis("Vertical");
                     return new Vector2(x, y).normalized;
                 });
+        }
 
+        private IObservable<Vector2> MouseObservable()
+        {
             // Mouse look ticks on Update
-            Mouselook = this.UpdateAsObservable()
-                .Select(_ => {
+            return this.UpdateAsObservable()
+                .Select(_ =>
+                {
                     var x = Input.GetAxis("Mouse X");
                     var y = Input.GetAxis("Mouse Y");
                     return new Vector2(x, y);
                 });
+        }
 
-            Run = this.UpdateAsObservable()
-                .Select(_ => Input.GetButton("Fire3"))
+        private ReadOnlyReactiveProperty<bool> RunFlag()
+        {
+            return this.UpdateAsObservable()
+                .Where(_ => Input.GetButtonDown("Fire 3"))
+                .Select(_ => _runValue = !_runValue)
                 .ToReadOnlyReactiveProperty();
         }
     }
