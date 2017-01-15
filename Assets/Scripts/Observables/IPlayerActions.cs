2df6afdc9ed918b16491a7337250d293e5496f1e
Yunspace <yun@yunspace.com>
2017-01-15 23:29:56 +1100

    downgrade to 5.5.0f3

--- Assets/Scripts/Observables/IPlayerActions.cs
+++ Assets/Scripts/Observables/IPlayerActions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using UniRx;
 using UnityEngine;
 
 namespace Assets.Scripts.Observables
--- Assets/Scripts/Observables/Inputs.cs
+++ Assets/Scripts/Observables/Inputs.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-using UniRx;
+﻿using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
 
