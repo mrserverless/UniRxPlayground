ee47fd9f990bbf788588d7577d423c6b62452747
Yunspace <yun@yunspace.com>
2017-01-19 23:09:37 +1100

    added PlayerService

--- Assets/Scripts/Models/Player.cs
+++ Assets/Scripts/Models/Player.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections.Generic;
 using ZeroFormatter;
 
-[ZeroFormattable]
-public class Player {
-    // Index is key of serialization
-    [Index(0)]
-    public virtual int Age { get; set; }
+namespace Models
+{
+    [ZeroFormattable]
+    public class Player
+    {
+        // Index is key of serialization
+        [Index(0)]
+        public virtual int Age { get; set; }
 
-    [Index(1)]
-    public virtual string FirstName { get; set; }
+        [Index(1)]
+        public virtual string FirstName { get; set; }
 
-    [Index(2)]
-    public virtual string LastName { get; set; }
+        [Index(2)]
+        public virtual string LastName { get; set; }
 
-    // When mark IgnoreFormatAttribute, out of the serialization target
-    [IgnoreFormat]
-    public string FullName { get { return FirstName + LastName; } }
+        // When mark IgnoreFormatAttribute, out of the serialization target
+        [IgnoreFormat]
+        public string FullName
+        {
+            get { return FirstName + LastName; }
+        }
 
-    [Index(3)]
-    public virtual IList<int> List { get; set; }
-
+        [Index(3)]
+        public virtual IList<int> List { get; set; }
+    }
 }
---
+++ Assets/Scripts/Services/PlayerService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ZeroFormatter;
+using Models;
+
+public class PlayerService  {
+
+    public byte[] SendPlayer(Player player)
+    {
+        var bytes = ZeroFormatterSerializer.Serialize(player);
+        return bytes;
+    }
+
+
+}
