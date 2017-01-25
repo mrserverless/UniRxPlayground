478d6e05bac58b5bc1ba533d022f064a5f61c7b6
Yun Zhi Lin <yun@yunspace.com>
2017-01-25 22:40:54 +1100

    websocketclient placeholder

---
+++ Assets/Scripts/Services/WebsocketSharpClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WebsocketSharpClient : IWebSocketClient {
+
+    public WebsocketSharpClient(string url)
+    {
+        using (var ws = new WebSocket (url)) {
+            ws.OnMessage += (sender, e) =>
+                Console.WriteLine ("Laputa says: " + e.Data);
+
+            ws.Connect ();
+            ws.Send ("BALUS");
+            Console.ReadKey (true);
+        }
+    }
+
+}
