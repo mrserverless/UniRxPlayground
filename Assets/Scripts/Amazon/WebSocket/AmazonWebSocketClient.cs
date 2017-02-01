b3b220a2fdf02a0b1363a82c243a882e33c695eb
Yun Zhi Lin <yun@yunspace.com>
2017-02-02 09:10:50 +1100

    added canonical headers and test

--- Assets/Scripts/Amazon/WebSocket/AmazonWebSocketClient.cs
+++ Assets/Scripts/Amazon/WebSocket/AmazonWebSocketClient.cs
@@ -20,45 +20,45 @@ namespace Amazon.WebSocket
 
         public string ComputeSignature(Uri host, Credentials credentials, RegionEndpoint region)
         {
-
             var signedAt = DateTime.UtcNow;
 
             var canonicalQueryString = GetCanonicalQueryString(credentials, region, signedAt);
-            GetCanonicalRequest(canonicalQueryString);
+            var canonicalHeaders = GetCanonicalHeaders(host);
+            var canonicalRequest = GetCanonicalRequest(canonicalQueryString, canonicalHeaders);
             return "";
         }
 
         // http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
-        private static string GetCanonicalRequest(string canonicalQueryString)
+        public string GetCanonicalRequest(string canonicalQueryString, string canonicalHeaders)
         {
             const string method = "GET";
             const string canonicalUri = "/mqtt";
 
-
             var canonicalRequest = new StringBuilder()
                 .AppendFormat("{0}\n", method)
                 .AppendFormat("{0}\n", canonicalUri)
-                .AppendFormat("{0}\n", canonicalQueryString);
-//            canonicalRequest.AppendFormat("{0}\n", GetCanonicalHeaders(request, signedHeaders));
+                .AppendFormat("{0}\n", canonicalQueryString)
+                .AppendFormat("{0}\n", canonicalHeaders);
 //            canonicalRequest.AppendFormat("{0}\n", String.Join(";", signedHeaders));
 //            canonicalRequest.Append(GetPayloadHash(request));
             return canonicalRequest.ToString();
         }
 
+        public string GetCanonicalHeaders(Uri host)
+        {
+            return "host:" + host.AbsolutePath + '\n';
+        }
+
         public string GetCanonicalQueryString(Credentials credentials, RegionEndpoint region, DateTime signedAt)
         {
+            var dateStamp = signedAt.ToString(ISO8601BasicDateFormat, CultureInfo.InvariantCulture);
             var amzdate = signedAt.ToString(ISO8601BasicDateTimeFormat, CultureInfo.InvariantCulture);
-
-            var dateStamp = signedAt.ToString(AWSSDKUtils.ISO8601BasicDateFormat, CultureInfo.InvariantCulture);
             var scope = string.Format(CultureInfo.InvariantCulture, "{0}/{1}/{2}/{3}", dateStamp, region.SystemName,
                 Service, Terminator);
 
-            var secretKey = credentials.SecretKey;
-            var accessKey = credentials.AccessKeyId;
-
             var queryString = new StringBuilder()
                 .Append("&X-Amz-Algorithm=" + Algorithm)
-                .Append("&X-Amz-Credential=" + accessKey + '/' + scope)
+                .Append("&X-Amz-Credential=" + credentials.AccessKeyId + '/' + scope)
                 .Append("&" + HeaderKeys.XAmzDateHeader + "=" + amzdate)
                 .Append("&X-Amz-Expires=86400")
                 .Append("&" + HeaderKeys.XAmzSignedHeadersHeader + "=host");
